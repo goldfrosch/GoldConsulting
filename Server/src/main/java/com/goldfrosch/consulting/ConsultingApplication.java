cf336e4907ab435b63f3d2a110797b02870dd773
goldfrosch <goldfrosch@daum.net>
2022-02-06 19:34:09 +0900

    GoldFrosch / back -> create test socket

--- Server/src/main/java/com/goldfrosch/consulting/ConsultingApplication.java
+++ Server/src/main/java/com/goldfrosch/consulting/ConsultingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsultingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConsultingApplication.class, args);
     }
-
 }
